35a82c67d91ee5457b4dddd7ce052ae6ac2cfe05
Ferhat Ucar <67374172+drferhatu@users.noreply.github.com>
2025-11-29 11:39:18 +0300

    Remove factorial function and main logic

--- ex13.c
+++ ex13.c
@@ -1,17 +1,1 @@
-#include <stdio.h>
 
-int factorial(int n) {
-    if(n <= 1) return 1;
-    return n * factorial(n-1);
-}
-
-int main() {
-    int n;
-    do {
-        printf("Enter a number (< 50): ");
-        scanf("%d", &n);
-    } while(n >= 50 || n < 0);
-    
-    printf("%d! = %d\n", n, factorial(n));
-    return 0;
-}
