37fdc432c40d74b608a14e039de080f88679c0c4
Ferhat Ucar <67374172+drferhatu@users.noreply.github.com>
2025-11-29 11:39:39 +0300

    Remove Fibonacci series code from ex14.c
    Remove the Fibonacci series implementation from ex14.c.

--- ex14.c
+++ ex14.c
@@ -1,19 +1,1 @@
-#include <stdio.h>
 
-int fibonacci(int n) {
-    if(n <= 1) return n;
-    return fibonacci(n-1) + fibonacci(n-2);
-}
-
-int main() {
-    int n;
-    printf("Enter position in Fibonacci series: ");
-    scanf("%d", &n);
-    
-    printf("Fibonacci series up to position %d:\n", n);
-    for(int i = 0; i <= n; i++) {
-        printf("%d ", fibonacci(i));
-    }
-    printf("\n");
-    return 0;
-}
