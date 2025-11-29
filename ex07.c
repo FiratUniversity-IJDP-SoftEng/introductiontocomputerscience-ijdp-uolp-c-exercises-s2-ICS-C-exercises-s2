aae457256c90b603b56c0a078492e627e8820cf8
Ferhat Ucar <67374172+drferhatu@users.noreply.github.com>
2025-11-29 11:36:40 +0300

    Remove main function and print statements

--- ex07.c
+++ ex07.c
@@ -1,10 +1,1 @@
-#include <stdio.h>
 
-int main() {
-    printf("Col1  Col2  Col3\n");
-    printf("----------------\n");
-    for(int i = 1; i <= 10; i++) {
-        printf("%-5d %-5d %d\n", i*5, 100-((i-1)*10+1), i);
-    }
-    return 0;
-}
